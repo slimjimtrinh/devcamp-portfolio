c32d85ae12cd427ae6c19f47eb11d8c80af8ec68
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 16:59:14 +0000

    Completed authentication with vurtual attrs for user model

--- DevcampPortfolio/app/models/user.rb
+++ DevcampPortfolio/app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates_presence_of :name
+  
+  def first_name ##name can be anything
+    self.name.split.first
+  end
+  
+  def last_name ##name can be anything
+    self.name.split.last
+
+  end
+
 end
