fe2d25ae4c05a64694b1e19de9dd8c25f58a9da1
Jordan Hudgens <jordan@devcamp.com>
2017-08-07 17:52:41 +0000

    Updated seeds file

--- DevcampPortfolio/db/seeds.rb
+++ DevcampPortfolio/db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "test@test.com",
+  password: "123456test",
+  password_confirmation: "123456test",
+  name: "Admin User",
+  roles: "site_admin"
+  )
+  
+puts "1 Admin User created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "123456test",
+  password_confirmation: "123456test",
+  name: "Regular User",
+  )
+  
+puts "1 Regular User created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
@@ -6,66 +26,55 @@ end
 
 puts "3 Topics created"
 
-10.times do |blog|
+4.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
-    quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-    voluptas sit aspernatur aut odit aut fugit,
-    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, 
-    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et 
-    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
-    ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-    Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-    molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    body: "Put text here",
   topic_id: Topic.last.id
     )
 end
 
-puts "10 blog posts created"
+puts "4 blog posts created"
 
-5.times do |skill|
+1.times do |skill|
   Skill.create!(
-    title: "Rails #{skill}",
-    percent_utalized: 12
+    title: "Rails",
+    percent_utalized: 50
     )
+  Skill.create!(
+    title: "High School",
+    percent_utalized: 80
+    )
+  Skill.create!(
+  title: "C+",
+  percent_utalized: 50
+  )
   end
   
-  puts "5 skills created"
+  puts "3 skills created"
   
   8.times do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}",
-      subtitle: "Ruby on Rails", 
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-      officia deserunt mollit anim id est laborum.",
+      subtitle: "Change Subtitle", 
+      body: "Add text here",
       main_image: "http://via.placeholder.com/600x400", 
       thumb_image: "http://via.placeholder.com/350x200"
       )
     end
   
-  1.times do |portfolio_item|
-    Portfolio.create!(
-      title: "Portfolio title: #{portfolio_item}",
-      subtitle: "Angular", 
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-      officia deserunt mollit anim id est laborum.",
-      main_image: "http://via.placeholder.com/600x400", 
-      thumb_image: "http://via.placeholder.com/350x200"
-      )
-    end
+  ##1.times do |portfolio_item|
+##    Portfolio.create!(
+##      title: "Portfolio title: #{portfolio_item}",
+##      subtitle: "Angular", 
+##      body: "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+##      officia deserunt mollit anim id est laborum.",
+##      main_image: "http://via.placeholder.com/600x400", 
+##      thumb_image: "http://via.placeholder.com/350x200"
+##      )
+##    end
   
-  puts "9 portfolio items created"
+  puts "8 portfolio items created"
   
   3.times do |techonology|
     Portfolio.last.techonologies.create!(
